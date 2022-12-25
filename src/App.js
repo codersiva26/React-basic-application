2b651574fbd934342c768da037b43b209a8ac12a
Siva Priya <priyapatnala26@gmail.com>
2022-12-25 11:10:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import One from './Components/One'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <One />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/One.js
@@ -0,0 +1,9 @@
+
+function One(){
+    return(
+        <div>
+            <h1>Hiii world</h1>
+        </div>
+    )
+}
+export default One;
